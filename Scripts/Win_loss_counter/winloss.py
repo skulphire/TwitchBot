ce746e3e04cd54b2916aa1015d52fb94c0bc7590
Creghton Kinney <cdkrobotics@gmail.com>
2017-03-03 15:40:06 -0500

    working on win loss counter

--- Scripts/Win_loss_counter/winloss.py
+++ Scripts/Win_loss_counter/winloss.py
@@ -1,8 +1,17 @@
 import os
 
+from msvcrt import getch
+
 class Winloss(object):
     def __init__(self):
-        self.usefile()
+        self.key = ord(getch())
+        self.win = 0
+        self.winPercent = 0
+        self.lose = 0
+        self.losePercent = 0
+        #self.usefile()
+
+
 
     def usefile(self):
         f = "winloss.txt"
@@ -11,3 +20,13 @@ class Winloss(object):
         except:
             print("File not found, creating...")
             file = open(f,'w')
+            self.writeFileInitial(file)
+
+    def writeFileInitial(self, file):
+        file.write("Win: %d(%d%%)      Loss: %d(%d%%)" % (self.win, self.winPercent, self.lose,self.losePercent))
+        file.close()
+
+    def counting(self):
+        while True:
+            #self.key = ord(getch())
+            print(ord(getch()))
