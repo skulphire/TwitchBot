bcd4ce19ad93f89dbaf82572002e3a3eb7e62de4
Creghton Kinney <cdkrobotics@gmail.com>
2017-03-03 17:13:05 -0500

    dont need pyhook, just working on calculations

--- Scripts/Win_loss_counter/winloss.py
+++ Scripts/Win_loss_counter/winloss.py
@@ -1,32 +1,44 @@
 import os
-import pyHook
-from msvcrt import getch
+
+
+
 
 class Winloss(object):
     def __init__(self):
-        self.key = ord(getch())
         self.win = 0
         self.winPercent = 0
         self.lose = 0
         self.losePercent = 0
-        #self.usefile()
+        self.f = "winloss.txt"
+        self.usefile()
 
 
 
     def usefile(self):
-        f = "winloss.txt"
+
         try:
-            file = open(f,'r')
+            file = open(self.f,'r')
+            self.counting()
         except:
             print("File not found, creating...")
-            file = open(f,'w')
-            self.writeFileInitial(file)
+            file = open(self.f,'w')
+            self.writeFile(file)
 
-    def writeFileInitial(self, file):
+    def writeFile(self, file):
         file.write("Win: %d(%d%%)      Loss: %d(%d%%)" % (self.win, self.winPercent, self.lose,self.losePercent))
         file.close()
 
     def counting(self):
         while True:
-            #self.key = ord(getch())
-            print(ord(getch()))
+            update = input("win(1),loss(2): ")
+            if update == "1":
+                self.win = self.win+1
+            elif update == "2":
+                self.lose = self.lose+1
+
+            self.winPercent = abs(((self.win-self.lose)/self.win)*100)
+            if not self.lose == 0:
+                self.losePercent = abs(self.winPercent-100)
+            file = open(self.f, 'w')
+            self.writeFile(file)
+            file.close()
