0cfb54875aca4793c66be76af90a047e468434b5
Creghton Kinney <cdkrobotics@gmail.com>
2017-03-08 15:50:31 -0500

    completed percent calcs

--- Scripts/Win_loss_counter/winloss.py
+++ Scripts/Win_loss_counter/winloss.py
@@ -18,7 +18,6 @@ class Winloss(object):
 
         try:
             file = open(self.f,'r')
-            self.counting()
         except:
             print("File not found, creating...")
             file = open(self.f,'w')
@@ -28,17 +27,17 @@ class Winloss(object):
         file.write("Win: %d(%d%%)      Loss: %d(%d%%)" % (self.win, self.winPercent, self.lose,self.losePercent))
         file.close()
 
-    def counting(self):
+    def countWinLoss(self):
         while True:
+            oldWin = self.win
             update = input("win(1),loss(2): ")
             if update == "1":
                 self.win = self.win+1
             elif update == "2":
                 self.lose = self.lose+1
 
-            self.winPercent = abs(((self.win-self.lose)/self.win)*100)
-            if not self.lose == 0:
-                self.losePercent = abs(self.winPercent-100)
+            self.winPercent = self.win/(self.win+self.lose)*100 #percent increase - abs(((self.win - oldWin)/oldWin)*100)
+            self.losePercent = abs(self.winPercent-100)
             file = open(self.f, 'w')
             self.writeFile(file)
             file.close()
--- WLcounter.py
+++ WLcounter.py
@@ -2,4 +2,4 @@ from Scripts.Win_loss_counter.winloss import Winloss
 
 if __name__ == '__main__':
     script = Winloss()
-    script.counting()
+    script.countWinLoss()
