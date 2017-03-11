148461db9f09209c52f02e36b2216f0d2353dd3a
Creghton <cdkrobotics@gmail.com>
2017-03-10 20:15:13 -0500

    updated win/loss

--- Scripts/Win_loss_counter/winloss.py
+++ Scripts/Win_loss_counter/winloss.py
@@ -27,20 +27,27 @@ class Winloss(object):
         file.write("Win: %d(%d%%)      Loss: %d(%d%%)" % (self.win, self.winPercent, self.lose,self.losePercent))
         file.close()
 
+    def percentages(self):
+        self.winPercent = self.win / (
+        self.win + self.lose) * 100  # percent increase - abs(((self.win - oldWin)/oldWin)*100)
+        self.losePercent = abs(self.winPercent - 100)
+        file = open(self.f, 'w')
+        self.writeFile(file)
+
     def countWinLoss(self):
         while True:
             oldWin = self.win
             update = input("win(1),loss(2): ")
             if update == "1":
                 self.win = self.win+1
+                self.percentages()
             elif update == "2":
                 self.lose = self.lose+1
+                self.percentages()
             elif update == "0":
                 self.lose = 0
                 self.win = 0
-
-            self.winPercent = self.win/(self.win+self.lose)*100 #percent increase - abs(((self.win - oldWin)/oldWin)*100)
-            self.losePercent = abs(self.winPercent-100)
-            file = open(self.f, 'w')
-            self.writeFile(file)
-            file.close()
+                self.winPercent = 0
+                self.losePercent = 0
+                file = open(self.f, 'w')
+                self.writeFile(file)
